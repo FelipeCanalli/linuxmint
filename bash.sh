46823867c023e231e3034dfe3492f71f996e553d
Felipe Galvão <53832209+FelipeGalvao1@users.noreply.github.com>
2019-08-06 17:11:28 -0300

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -14,26 +14,3 @@ cd ______                (abre o diretorio)
 pwd                      ( diretorio corrente )
 mkdir _____                (criar diretorio)
 &&                       (se o 1º comando for excecutado com sucesso ir para o 2º)
-echo __________                    (imprimi)
-> ( manda para um diretorio, se ele não existir ele cria )
-exemplo : _______________ > __________.sh
-modifica local e data
-exemplo : touch senac.sh
-
-cat _____                            ( mostra o conteudo do arquivo )
-cat -n                           ( mostra o número de linhas ) 
-
-head _____                 (listar o cabeçario do arquivo) apenas 10 linhas
-head -nnº ______ (quantas linhas quer ver)
-
-
-tail   _______          ( mostra o final do arquivo)
-tail -n nº ______ (quantas linhas quer ver)
-
-less LICENSE (paginador) sai com o Q
-|            ( o final de um é o começo do outro )
-
-
-
-
-cat -n LICENSE| head -n200 | tail -n101 | less | echo > senac.sh
---
+++ comandos.sh
@@ -0,0 +1,58 @@
+    1  root
+    2  man sudo_root[
+    3  man sudo_root
+    4  ls
+    5  ls -lh
+    6  cd /
+    7  clear
+    8  cd
+    9  cd /
+   10  ls
+   11  ls -lh
+   12  cd -
+   13  clear
+   14  pwsd
+   15  pwd
+   16  cd/
+   17  cd / 
+   18  twd
+   19  pwd
+   20  cd
+   21  mkdir linuxmint
+   22  ls
+   23  cd linuxmint/
+   24  cd
+   25  cd linuxmint/
+   26  ls
+   27  clear
+   28  sudo apt update && sudo apt install git
+   29  git
+   30  git clone https://github.com/FelipeGalvao1/linuxmint.git
+   31  ls
+   32  cd linuxmint/
+   33  ls
+   34  ls -lh
+   35  clear
+   36  echo
+   37  cd
+   38  cls
+   39  ls
+   40  clear
+   41  echo
+   42  echo PERFECT K.O
+   43  echo opa
+   44  cls
+   45  clear
+   46  echo a
+   47  clear
+   48  ls
+   49  cd linuxmint/
+   50  cat -n | head -n 100 | tail -n 100 | less | echo > senac.sh
+   51  ,
+   52  cat -n LICENSE| head -n100 | tail -n200 | less
+   53  cat -n LICENSE| head -n200 | tail -n100 | less | echo > senac.sh
+   54  cat -n LICENSE| head -n200 | tail -n100 | less
+   55  cat -n LICENSE| head -n200 | tail -n101 | less
+   56  HISTORY
+   57  history
+   58  history > comandos.sh
