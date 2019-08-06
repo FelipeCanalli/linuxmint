dc7fffb7328b73ebac293a5af56e565edca48c2d
Felipe Galvão <53832209+FelipeGalvao1@users.noreply.github.com>
2019-08-06 15:22:37 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,15 @@
 Felipe Galvão sendo guiado pelo mestre Vaamonde
+
+github
+ctrl alt f1 sem ambiente gráfico
+e para voltar ctrl alt f7
+
+
+comandos do shell :
+ls                  ( lista os diretorios )
+ls -lh                  (modo humano)
+cd -                          (volta)
+cd ______                (abre o diretorio)
+
+pwd                      ( diretorio corrente )
+mkdir _____                (criar diretorio)
