714446686b53faf3635d088181f7fc1e38ed49ac
Felipe Galvão <53832209+FelipeGalvao1@users.noreply.github.com>
2019-08-06 16:28:06 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -14,4 +14,8 @@ cd ______                (abre o diretorio)
 pwd                      ( diretorio corrente )
 mkdir _____                (criar diretorio)
 &&                       (se o 1º comando for excecutado com sucesso ir para o 2º)
-echo                    
+echo __________                    (imprimi)
+> ( manda para um diretorio, se ele não existir ele cria )
+exemplo : _______________ > __________.sh
+modifica local e data
+exemplo : touch senac.sh
